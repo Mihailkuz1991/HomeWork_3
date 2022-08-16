018ba7316f3624426dfcb6322ea9a749782d46fc
Michael Kuznetsov <mihasyuga@yandex.ru>
2022-08-16 21:00:40 +0300

    HomeWork 3

---
+++ Z_23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear(); 
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите число -> {number}");
+int num = number; 
+
+void PrintQuart(int num)
+{
+    for (int i = 1; i <= num; i++)
+    Console.WriteLine($"{i} \t {i*i*i}");
+}
+if (num < 1)
+Console.WriteLine("Введите число > 1");
+
+PrintQuart(num);
